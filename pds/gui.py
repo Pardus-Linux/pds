2f93ac3bf0d6401489a67a3d3a3a50b20433aeda
Gökmen Göksel <gokmen@pardus.org.tr>
2010-09-24 09:40:57 +0000

    * Callback functions problem is now fixed  * Its ready to use  * Documentation added

--- pds/gui.py
+++ pds/gui.py
@@ -32,96 +32,128 @@ OVERLAY_OPACITY = 200
 
 class PAbstractBox(QtGui.QWidget):
     def __init__(self, parent):
+
+        # Overlay widget, it should be initialized at first
         self.overlay = QtGui.QWidget(parent)
         self.overlay.hide()
         self.__overlay_enabled = False
-        QtGui.QWidget.__init__(self, parent)
 
+        # Main widget initializing on parent widget
+        QtGui.QWidget.__init__(self, parent)
+        self.hide()
+
+        # Pre-defined states
         self.last_direction = IN
         self.last_move_direction = FORWARD
         self.last_start = TOPCENTER
         self.last_stop = BOTCENTER
         self.duration = 2000
 
+        # Override parents resize-event
         self.parent = parent
         self.parent.resizeEvent = self.resizeCallBacks
+
+        # Timeline for X coordinate
         self.sceneX = QtCore.QTimeLine()
+
+        # Timeline for Y coordinate
         self.sceneY = QtCore.QTimeLine()
+
+        # Timeline for fade-effect of overlay
         self.sceneF = QtCore.QTimeLine()
+
+        # Animation, QEasingCurve.Type
         self.animation = 30
+
+        # Callback functions for using at pre-defined statements
         self.call_back_functions = {IN:[], OUT:[], RESIZE:[]}
 
+        # Dummy function
         self.registerFunction(RESIZE, lambda: '')
-        self.registerFunction(OUT, lambda: self.flushCallBacks(RESIZE))
-        self.registerFunction(OUT, lambda: self.registerFunction(RESIZE, lambda: ''))
-
-    def flushCallBacks(self, direction):
-        self.call_back_functions[direction] = []
 
     def enableOverlay(self, animated = False):
+        # Resize the overlay with parent's size
         self.overlay.resize(self.parent.size())
         self.__overlay_enabled = True
         self.sceneF.setUpdateInterval(20)
+
+        # When animation finished, overlay animation should be stop
         self.registerFunction(IN,  self.sceneF.stop)
+
         if animated:
+            # Register animation range for overlay fade-in/out effect
             self.sceneF.setFrameRange(0, 200)
             self.sceneF.frameChanged.connect(lambda x: self.overlay.setStyleSheet('background-color: rgba(0,0,0,%s)' % x))
-            self.registerFunction(IN,  lambda: self.sceneF.setFrameRange(200, 0))
-            self.registerFunction(OUT, lambda: self.sceneF.setFrameRange(0, 200))
+            self.registerFunction(IN,  lambda: self.sceneF.setFrameRange(0, 200))
+            self.registerFunction(OUT, lambda: self.sceneF.setFrameRange(200, 0))
         else:
+            # Set overlay opacity
             self.overlay.setStyleSheet('background-color: rgba(0, 0, 0, %s)' % OVERLAY_OPACITY)
-
-        self.registerFunction(RESIZE, lambda: self.overlay.resize(self.parent.size()))
-        self.registerFunction(RESIZE, lambda: '')
 
     def disableOverlay(self):
         self.__overlay_enabled = False
 
     def resizeCallBacks(self, event):
+
+        # Run resize callbacks and parent widget's resizeEvent
         QtGui.QWidget(self.parent).resizeEvent(event)
+        if self.__overlay_enabled:
+            self.overlay.resize(self.parent.size())
         self.runCallBacks(RESIZE)
 
     def animate(self, direction = IN, move_direction = FORWARD, start = TOPCENTER, stop = BOTCENTER, start_after = None, duration = 0):
         if start_after:
+            # If there is an animation started before this one, we can easily start it when the old one finishes
             start_after.finished.connect(lambda: self.__animate(direction, move_direction, start, stop, duration))
         else:
+            # Otherwise, run the animation directly and return the timeline obj for using as a reference for later animations
             return self.__animate(direction, move_direction, start, stop, duration)
 
     def __animate(self, direction, move_direction, start, stop, duration):
 
+        # Stop all running animations
         self.sceneX.stop()
         self.sceneY.stop()
         self.sceneF.stop()
 
+        # Use given duration time or use the default one
         duration = duration if duration > 0 else self.duration
 
+        # Pop the dummy resize function and add a new animation function for repositioning the widget when resize occured.
         self.call_back_functions[RESIZE].pop()
         self.registerFunction(RESIZE, lambda: self.animate(self.last_direction,
                                                            self.last_move_direction,
                                                            CURRENT,
                                                            self.last_stop))
 
+        # Set last used animations with given values
         self.last_stop           = stop
         self.last_start          = start
         self.last_move_direction = move_direction
         self.last_direction      = direction
 
+        # Set X coordinate timeline settings
         self.sceneX.setDirection(move_direction)
         self.sceneX.setEasingCurve(QtCore.QEasingCurve(self.animation))
         self.sceneX.setDuration(duration)
         self.sceneX.setUpdateInterval(20)
 
+        # Set Y coordinate timeline settings
         self.sceneY.setDirection(move_direction)
         self.sceneY.setEasingCurve(QtCore.QEasingCurve(self.animation))
         self.sceneY.setDuration(duration)
         self.sceneY.setUpdateInterval(20)
+
+        # Update duration for overlay fade effect
         self.sceneF.setDuration(duration)
 
+        # Get current sizes
         p_width  = self.parent.width()
         p_height = self.parent.height()
         width  = self.width()
         height = self.height()
 
+        # Calculate new positions for given points
         limits = {TOPLEFT   : [0, 0],
                   TOPCENTER : [p_width/2 - width/2, 0],
                   TOPRIGHT  : [p_width - width, 0],
@@ -133,12 +165,14 @@ class PAbstractBox(QtGui.QWidget):
                   BOTRIGHT  : [p_width - width, p_height - height],
                   CURRENT   : [self.x(), self.y()]}
 
+        # Get start and stop positions
         start_pos = limits[start]
         stop_pos  = limits[stop]
 
         # Poor developer's debug mechanism.
         # print start_pos, stop_pos, width, height
 
+        # Update start and stop positions for given direction
         if direction == IN:
             self.show()
             if start in (TOPLEFT, MIDLEFT, BOTLEFT):
@@ -159,61 +193,74 @@ class PAbstractBox(QtGui.QWidget):
             elif stop == BOTCENTER:
                 stop_pos[1] += height
 
+        # Move the widget to calculated start position
         self.move(start_pos[0], start_pos[1])
 
+        # Set calculated start and stop positions
         self.sceneX.setFrameRange(start_pos[0], stop_pos[0])
         self.sceneX.frameChanged.connect(lambda x: self.move(x, self.y()))
-        self.sceneX.finished.connect(lambda: self.setHidden(direction == OUT))
-        self.sceneX.finished.connect(lambda: self.runCallBacks(direction))
-
         self.sceneY.setFrameRange(start_pos[1], stop_pos[1])
         self.sceneY.frameChanged.connect(lambda y: self.move(self.x(), y))
 
+        # Hide widget and reset resize callback functions when direction is OUT
+        self.sceneX.finished.connect(lambda: self.setHidden(direction == OUT))
+        self.sceneX.finished.connect(lambda: self.flushCallBacks(RESIZE, direction == OUT))
+
+        # Run predefined callback functions for given direction
+        self.runCallBacks(direction)
+
+        # Show/hide overlay if overlay enabled
         if self.__overlay_enabled:
             self.overlay.show()
             self.sceneX.finished.connect(lambda: self.overlay.setHidden(direction == OUT))
         else:
             self.overlay.hide()
 
+        # Start the animation !
         if self.sceneX.state() == QtCore.QTimeLine.NotRunning:
             self.sceneX.start()
             self.sceneY.start()
             if not start == CURRENT:
+                # If start position is CURRENT, it means the animation
+                # will just work for repositioning the widget,
+                # so we dont need overlay fade animation
                 self.sceneF.start()
 
+        # Return the X coordinate timeline obj to use as reference for next animation
         return self.sceneX
 
+    def flushCallBacks(self, direction, approve = False):
+        if approve:
+            # If approved reset given direction's call backs
+            self.call_back_functions[direction] = []
+            if direction == RESIZE:
+                # Dummy function
+                self.registerFunction(RESIZE, lambda: '')
+
     def registerFunction(self, direction, func):
+        # Add function to given direction's list
         if not func in self.call_back_functions[direction]:
             self.call_back_functions[direction].append(func)
 
     def runCallBacks(self, direction):
+        # Run all functions for given direction
         for func in self.call_back_functions[direction]:
             func()
-
-class PWidgetbox(PAbstractBox):
-    def __init__(self, parent, widget):
-        PAbstractBox.__init__(self, parent)
-
-class PInfoBox(PAbstractBox):
-    def __init__(self, parent=None):
-        PAbstractBox.__init__(self, parent)
-        self.label = QtGui.QLabel('Hello World !', self)
 
 class PMessageBox(PAbstractBox):
 
     # STYLE SHEET
     STYLE = """background-color:rgba(0,0,0,120);
                color:white;
-               border: 1px solid #FFF;
+               border: 1px solid #999;
                border-radius: 4px;"""
 
     def __init__(self, parent=None):
         PAbstractBox.__init__(self, parent)
         self.label = QtGui.QLabel(self)
         self.setStyleSheet(PMessageBox.STYLE)
-        self.padding_w = 14
-        self.padding_h = 8
+        self.padding_w = 18
+        self.padding_h = 12
         self.hide()
 
     def showMessage(self, message, duration = 2, inPos = MIDLEFT, stopPos = MIDCENTER, outPos = MIDRIGHT):
--- pds/test-gui.py
+++ pds/test-gui.py
@@ -1,7 +1,26 @@
 # -*- coding: utf-8 -*-
 
 from PyQt4 import QtCore, QtGui
-from pds.gui import PMessageBox
+from pds.gui import *
+from pds.progress import Ui_ProgressDialog
+
+class PWidgetbox(PAbstractBox):
+    def __init__(self, parent, widget):
+        PAbstractBox.__init__(self, parent)
+        ui = widget()
+        ui.setupUi(self)
+        self.enableOverlay()
+
+    def showAnimated(self):
+        self.animate(start = TOPCENTER, stop = MIDCENTER)
+
+    def hideAnimated(self):
+        self.animate(start = MIDCENTER, stop = BOTCENTER, direction = OUT)
+
+class PInfoBox(PAbstractBox):
+    def __init__(self, parent=None):
+        PAbstractBox.__init__(self, parent)
+        self.label = QtGui.QLabel('Hello World !', self)
 
 class Ui_Form(object):
     def setupUi(self, Form):
@@ -15,23 +34,28 @@ class Ui_Form(object):
         spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
         self.gridLayout.addItem(spacerItem, 0, 1, 1, 1)
         self.button = QtGui.QPushButton(Form)
+        self.button2 = QtGui.QPushButton(Form)
         self.button.setObjectName("button")
         self.gridLayout.addWidget(self.button, 0, 2, 1, 1)
+        self.gridLayout.addWidget(self.button2, 0, 3, 1, 1)
         self.webView = QtWebKit.QWebView(Form)
         self.webView.setUrl(QtCore.QUrl("http://developer.pardus.org.tr/"))
         self.webView.setObjectName("webView")
-        self.gridLayout.addWidget(self.webView, 1, 0, 1, 3)
+        self.gridLayout.addWidget(self.webView, 1, 0, 1, 4)
 
         self.retranslateUi(Form)
 
-        self.info = PMessageBox(Form)
-        self.button.clicked.connect(lambda: self.info.showMessage(self.line.text()))
+        self.info = PWidgetbox(Form, Ui_ProgressDialog)
+        self.button.clicked.connect(self.info.showAnimated)
+        self.button2.clicked.connect(self.info.hideAnimated)
+
         QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QtGui.QApplication.translate("Form", "Form", None, QtGui.QApplication.UnicodeUTF8))
         self.line.setText(QtGui.QApplication.translate("Form", "This is a test message from PDS GUI !", None, QtGui.QApplication.UnicodeUTF8))
         self.button.setText(QtGui.QApplication.translate("Form", "Show Message", None, QtGui.QApplication.UnicodeUTF8))
+        self.button2.setText(QtGui.QApplication.translate("Form", "Hide Message", None, QtGui.QApplication.UnicodeUTF8))
 
 from PyQt4 import QtWebKit
 
