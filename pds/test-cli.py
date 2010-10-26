44094c3293154676fca9c976d53d6a7cd74588a4
Gökmen Göksel <gokmen@pardus.org.tr>
2010-10-26 20:19:27 +0000

    * Add QProgressIndicator which is Qt Painter based animated busy indicator instead of using ugly gifs.

--- pds/test-cli.py
+++ pds/test-cli.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# Pardus Desktop Services
+# Copyright (C) 2010, TUBITAK/UEKAE
+# 2010 - Gökmen Göksel <gokmen:pardus.org.tr>
+
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+
 import time
-a=time.time()
+a = time.time()
+
 from pds import Pds
 
-b=Pds('package-manager')
+b = Pds('package-manager')
 
 print 'Current Desktop Environment         :', b.session.Name
 print 'Current Desktop Environment Version :', b.session.Version
--- pds/test-gui.py
+++ pds/test-gui.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 
+# Pardus Desktop Services
+# Copyright (C) 2010, TUBITAK/UEKAE
+# 2010 - Gökmen Göksel <gokmen:pardus.org.tr>
+
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+
 from PyQt4 import QtCore, QtGui
 from pds.gui import *
 from pds.progress import Ui_ProgressDialog
